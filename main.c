61a69fef3944fe6b0d3ea9f891053f618c1a1eba
chadmv95 <vredevch@mail.gvsu.edu>
2018-07-19 12:46:26 -0400

    added md5 hashing
    tested working in main
    need to add hashing to tree nodes now

--- main.c
+++ main.c
@@ -1,13 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "Node.h"
+#include "md5.h"
 
+#define SHA_DIGEST_LENGTH 32
 
+char *str2md5(const char*, int );
 
-int main()
+int main(int argc, char **argv)
 {
-    struct node *root = 0;
-
-    printf("Hello world!\n");
+    char *output = str2md5("Hello World", strlen("Hello World"));
+    printf("%s\n", output);
+    free(output);
     return 0;
+}
+
+char *str2md5(const char *str, int length) {
+    int n;
+    MD5_CTX c;
+    unsigned char digest[16];
+    char *out = (char*)malloc(33);
+
+    MD5_Init(&c);
+
+    while (length > 0) {
+        if (length > 512) {
+            MD5_Update(&c, str, 512);
+        } else {
+            MD5_Update(&c, str, length);
+        }
+        length -= 512;
+        str += 512;
+    }
+
+    MD5_Final(digest, &c);
+
+    for (n = 0; n < 16; ++n) {
+        snprintf(&(out[n*2]), 16*2, "%02x", (unsigned int)digest[n]);
+    }
+
+    return out;
 }
