442ee10da87b9d4b11613e76665c658790c4a329
chadmv95 <vredevch@mail.gvsu.edu>
2018-07-28 09:25:18 -0400

    added file opening capabilities

--- main.c
+++ main.c
@@ -2,8 +2,10 @@
 #include <stdlib.h>
 #include "Node.h"
 #include "md5.h"
+#include <string.h>
 
-char *str2md5(const char*, int );
+char* str2md5(const char*, int );
+char* readFile(char *filename);
 
 int main(int argc, char **argv)
 {
@@ -12,6 +14,15 @@ int main(int argc, char **argv)
     char *output = str2md5(file, strlen(file));
     printf("%s\n", output);
     free(output);
+
+    char *file3 = readFile("test1.txt");
+    char *file4 = readFile("test2.txt");
+
+    char *hash3 = str2md5(file3, strlen(file3));
+    char *hash4 = str2md5(file4, strlen(file4));
+    char *tophash1 = str2md5(strcat(hash3, hash4), strlen(strcat(hash3,hash4)));
+    printf("Top Hash: %s\n", tophash1);
+    printf("          1cd0b9f641ac298438582651bd56b22f\n");
 
     //struct node* root = buildTree();
 
@@ -35,6 +46,49 @@ int main(int argc, char **argv)
     return 0;
 }
 
+char* readFile(char *filename)
+{
+   char *buffer = NULL;
+   int string_size, read_size;
+   FILE *handler = fopen(filename, "r");
+
+   if (handler)
+   {
+       // Seek the last byte of the file
+       fseek(handler, 0, SEEK_END);
+       // Offset from the first to the last byte, or in other words, filesize
+       string_size = ftell(handler);
+       // go back to the start of the file
+       rewind(handler);
+
+       // Allocate a string that can hold it all
+       buffer = (char*) malloc(sizeof(char) * (string_size + 1) );
+
+       // Read it all in one operation
+       read_size = fread(buffer, sizeof(char), string_size, handler);
+
+       // fread doesn't set it so put a \0 in the last position
+       // and buffer is now officially a string
+       buffer[string_size] = '\0';
+
+       if (string_size != read_size)
+       {
+           // Something went wrong, throw away the memory and set
+           // the buffer to NULL
+           free(buffer);
+           buffer = NULL;
+       }
+
+       // Always remember to close the file.
+       fclose(handler);
+    }
+
+    return buffer;
+}
+
+/*
+this helper function was found online, url to forum has since been lost
+*/
 char *str2md5(const char *str, int length) {
     int n;
     MD5_CTX c;
